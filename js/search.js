d4f261060c6fa12914d6be0aef7f5f16eb68ab74
prabjot101 <48800723+prabjot101@users.noreply.github.com>
2022-04-12 11:06:53 -0700

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,33 @@
+var maximages = 5;
+var startpath = "pix3/birds"
+var extension = ".png"
+function calcslide(x) {
+    //Get file name of image that's showing.
+    var currentimage = document.getElementById('bigpic').src;
+    var path = 'pix3/birds' + z + '.png';
+    var z = '0' + x;
+    var y = '' + 10;
+    //Locate file name extension in current image source string.
+    var dotat = currentimage.indexOf(extension);
+    //Grab two digits to the left of that file name extension.
+    var stringnumber = currentimage.substr(dotat - 2, 2);
+    //Convert stringnumber string to number and add x.
+    var nextnum = parseInt(stringnumber) + x;
+    //If nextnum is less than 1, wrap around to maximages.
+    if (nextnum < 1) {
+    nextnum = maximages;
+    }
+    //If nextnum is greater than maximages, wrap around to 1.
+    if (nextnum > maximages) {
+    nextnum = 1;
+    }
+    //Create two-digit string from number (leading zero if less than 10).
+    var twodigitnum = ('0' + nextnum).slice(-2);
+    //Create new file name from two-digit number string.
+    var showimg = startpath + twodigitnum + extension;
+    showbig(showimg);
+}
+function showbig(pic) {
+
+   document.getElementById("bigpic").src = pic;
+}
